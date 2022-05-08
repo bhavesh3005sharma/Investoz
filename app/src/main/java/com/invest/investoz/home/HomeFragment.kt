33760bc66959170982ef710fbabf0e6aea20b577
bhavesh3005sharma <bhavesh3005sharma@gmail.com>
2022-05-08 10:38:05 +0530

    Add : Home Page UI

--- app/src/main/java/com/invest/investoz/home/HomeFragment.kt
+++ app/src/main/java/com/invest/investoz/home/HomeFragment.kt
@@ -5,16 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.invest.investoz.adapter.NewsAdapter
+import com.invest.investoz.adapter.StartupAdapter
 import com.invest.investoz.adapter.StartupTrendAdapter
 import com.invest.investoz.databinding.FragmentHomeBinding
+import com.invest.investoz.model.NewsModel
 import com.invest.investoz.model.StartupModel
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
-    private val startupModel = StartupModel("88","erfva","efwaedf","fed","ewfds","Groww")
-    private val list = mutableListOf(startupModel)
-    private val adapter = StartupTrendAdapter(list)
+    private val startupModel = StartupModel("88","Kylo Apps is a software company based out of Delhi, India, offering software development services to clients all over the world. \n","2.56L","https://ibb.co/KDCMjsJ","24.55","Kylo Apps","34.78%",false)
+    private val startUpTrendList = mutableListOf(startupModel)
+    private val startUpsList = mutableListOf(startupModel)
+    private val newsList = mutableListOf(NewsModel("","","",),NewsModel("","","",),NewsModel("","","",),NewsModel("","","",))
+    private val startupTrendAdapter = StartupTrendAdapter(startUpTrendList)
+    private val startupAdapter = StartupAdapter(startUpsList)
+    private val newsAdapter = NewsAdapter(newsList)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,8 +29,11 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
 
-        for (i in 0..4) { list.add(startupModel) }
-        binding.rvStartupsTrend.adapter = adapter
+        for (i in 0..4) { startUpTrendList.add(startupModel) }
+        startUpsList.addAll(startUpTrendList)
+        binding.rvStartupsTrend.adapter = startupTrendAdapter
+        binding.rvStartups.adapter = startupAdapter
+        binding.rvNews.adapter = newsAdapter
 
         return binding.root
     }
--- app/src/main/java/com/invest/investoz/model/StartupModel.kt
+++ app/src/main/java/com/invest/investoz/model/StartupModel.kt
@@ -1,10 +1,23 @@
 package com.invest.investoz.model
 
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.invest.investoz.R
+import com.squareup.picasso.Picasso
+
 data class StartupModel(
     val _id: String,
     val about: String,
     val evaluation: String,
     val logo: String,
     val minAmmount: String,
-    val name: String
+    val name: String,
+    val percentageEquity : String,
+    val goingUp : Boolean
 )
+
+@BindingAdapter("android:loadImage")
+fun loadImage(image : AppCompatImageView, url: String) {
+    if(url.isNotEmpty())
+        Picasso.get().load(url).placeholder(R.drawable.startup_img).into(image)
+}
